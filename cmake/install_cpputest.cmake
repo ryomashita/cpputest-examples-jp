4dac846d07917d30cf6009b1be29c7e71e86d577
ryomashita <86085692+ryomashita@users.noreply.github.com>
2023-10-23 14:59:37 +0900

    add cmake/install_cpputest.cmake

---
+++ cmake/install_cpputest.cmake
@@ -0,0 +1,28 @@
+# CppUTest
+include(FetchContent)
+FetchContent_Declare(
+    CppUTest
+    GIT_REPOSITORY https://github.com/cpputest/cpputest.git
+    GIT_TAG        master # or use release tag, eg. v4.0
+)
+# Set this to ON if you want to have the CppUTests in your project as well.
+set(TESTS OFF CACHE BOOL "Switch off CppUTest Test build")
+FetchContent_MakeAvailable(CppUTest)
+
+# link CppUTest to a target
+function(link_cpputest target)
+  target_link_libraries(target PRIVATE
+    CppUTest::CppUTest
+    CppUTest::CppUTestExt)
+endfunction()
+
+# add target to CTest's test suite
+function(add_cpputest target)
+  include(CTest)
+  add_test(target target)
+endfunction()
+
+function(link_and_addtest_cpputest target)
+  link_cpputest(${target})
+  add_cpputest(${target})
+endfunction()
