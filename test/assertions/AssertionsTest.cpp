86e968ec48fea042bd2aa11f72fcc3a20d678d6c
ryomashita <86085692+ryomashita@users.noreply.github.com>
2023-11-07 10:31:34 +0900

    add test case in AssertionsTest.cpp

--- test/assertions/AssertionsTest.cpp
+++ test/assertions/AssertionsTest.cpp
@@ -39,12 +39,48 @@ TEST(AssertionsTest, BYTES_EQUAL) {
   BYTES_EQUAL(0xab, 0xab);
   BYTES_EQUAL(0x1234'5678, 0x78);
 }
+TEST(AssertionsTest, POINTERS_EQUAL) {
+  // POINTERS_EQUAL(expected, actual) - Compares two const void *
+  int a = 1;
+  int &b = a;
+  POINTERS_EQUAL(&a, &b);
+}
+TEST(AssertionsTest, DOUBLES_EQUAL) {
+  // DOUBLES_EQUAL(expected, actual, tolerance) - Compares two doubles within
+  // some tolerance
+  DOUBLES_EQUAL(1.7, (double)85 / 50, 0);
+  DOUBLES_EQUAL((double)1 / 3, 0.333, 0.001);
+}
+TEST(AssertionsTest, ENUMS_EQUAL_INT) {
+  // ENUMS_EQUAL_INT(excepted, actual) - Compares two enums which their
+  enum MyEnum1 { A1, B1, C1 };
+  ENUMS_EQUAL_INT(A1, A1);
 
-// [TODO]
-// POINTERS_EQUAL(expected, actual) - Compares two const void *
-// DOUBLES_EQUAL(expected, actual, tolerance) - Compares two doubles within some
-// tolerance ENUMS_EQUAL_INT(excepted, actual) - Compares two enums which their
-// underlying type is int ENUMS_EQUAL_TYPE(underlying_type, excepted, actual) -
-// Compares two enums which they have the same underlying type FAIL(text) -
-// always fails TEST_EXIT - Exit the test without failure - useful for contract
-// testing (implementing an assert fake)
+  /* intにcastして比較されるので、上位桁は無視される */
+  enum class MyEnum2 : uint64_t {
+    A = 0xffff'ffff'1234'5678,
+    B = 0x1234'5678,
+  };
+  ENUMS_EQUAL_INT(MyEnum2::A, MyEnum2::B);
+}
+TEST(AssertionsTest, ENUMS_EQUAL_TYPE) {
+  // ENUMS_EQUAL_TYPE(underlying_type, excepted, actual) - Compares two enums
+  // which they have the same underlying type
+  enum class MyEnum1 : uint64_t {
+    A = 0xffff'ffff'1234'5678,
+    B = 0x1234'5678,
+    C = 0xffff'ffff'1234'5678,
+  };
+  // ENUMS_EQUAL_TYPE(uint64_t, MyEnum1::A, MyEnum1::B); // failed
+  ENUMS_EQUAL_TYPE(uint64_t, MyEnum1::A, MyEnum1::C);
+}
+TEST(AssertionsTest, FAIL) {
+  // FAIL(text) - always fails
+  if (false) FAIL("This is a failure");
+}
+TEST(AssertionsTest, TEST_EXIT) {
+  // TEST_EXIT - Exit the test without failure - useful for contract testing
+  // (implementing an assert fake)
+  TEST_EXIT;
+  FAIL("won't reach here");
+}
